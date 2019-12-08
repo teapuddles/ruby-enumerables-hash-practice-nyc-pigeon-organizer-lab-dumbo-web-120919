b4f25494e0050ff954a359dfc9a6b40859872020
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-08 00:34:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each do |pigeon_attributes, values|
     values.each do |value, array|
       array.each do |name|
-        if new_pigeon_hash == nil 
-          new_pigeon_hash =
+        if new_pigeon_hash[name] == nil 
+          new_pigeon_hash[name] = {}
 end
