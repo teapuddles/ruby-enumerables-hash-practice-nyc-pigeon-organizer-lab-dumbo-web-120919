7ee6ba69a3f2d2fa2b1cb010f6e48465612cb908
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-08 01:09:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  data.each do |pigeon_attributes, values|
+    values.each do |value, array|
+      array.each do |name|
+        if new_pigeon_hash[name] == nil    ##why tho?
+          new_pigeon_hash[name] = {}
+          new_pigeon_hash[name][pigeon_attributes] = []
+        else
+          new_pigeon_hash[name][pigeon_attributes] = []
+        end
+      end
+    end
+  end
+  new_pigeon_hash.each do |name, values|
+    values.each do |single_attribute, array|
+      data.each do |pigeon_attributes, values|
+        values.each do |value, array|
+          array.each do |element|
+            if element == name && single_attribute == pigeon_attributes
+              new_pigeon_hash[name][single_attribute] << value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  return new_pigeon_hash
 end
+    
